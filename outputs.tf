e3c733142acc5dc48e20b3ad5342868dccf011d5
Vladislav Naydenov <v.naydenov@icloud.com>
2023-10-12 15:49:57 +0300

    Add description for the outputs

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,10 @@
 output "kubeconfig" {
   value     = azurerm_kubernetes_cluster.k8s.kube_config_raw
+  description = "The kubeconfig for the AKS cluster"
   sensitive = true
 }
 
 output "ingress_fqdn" {
     value = azurerm_public_ip.k8s.fqdn
+    description = "The FQDN of the ingress controller"
 }
